ab8a24fcc352345d9619eb61ef9614fad2676a23
Pratik Mishra <91556360+pratikmishra17@users.noreply.github.com>
2025-03-15 12:27:37 -0400

    Update maximum_count_of_postive_and_negative.java

--- maximum_count_of_postive_and_negative.java
+++ maximum_count_of_postive_and_negative.java
@@ -1,16 +1,23 @@
 import java.lang.*;
-class Solution {
+
+public class Solution {
     public int maximumCount(int[] nums) {
         int pos = 0;
         int neg = 0;
         int l = nums.length;
-        for(int i=0; i<l; i++){
-            if(nums[i] > 0){
+        for (int i = 0; i < l; i++) {
+            if (nums[i] > 0) {
                 pos++;
-            }else if(nums[i] < 0){
+            } else if (nums[i] < 0) {
                 neg++;
             }
         }
-        return Math.max(pos,neg);
+        return Math.max(pos, neg);
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = {-3, -2, -1, 0, 1, 2, 3};
+        System.out.println("Maximum count of positive or negative numbers: " + sol.maximumCount(nums));
     }
 }
